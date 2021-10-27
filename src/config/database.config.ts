dde65f60fb7eac29e8589072bede9b7fc29dc802
FSolM <fersolmarin@gmail.com>
2021-10-27 10:34:24 -0500

    env Variables Configuration Fixed

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,5 @@
-import { ConfigService } from '@nestjs/config';
-
-export const mongoFactory = async (ConfigService: ConfigService) => ({
-  uri: ConfigService.get('MONGODB_URL'),
+export const mongoFactory = async () => ({
+  uri: process.env.MONGODB_URL,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
