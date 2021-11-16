064a876c472e393fa9c1fd2e62daf48b47734a4b
FSolM <fersolmarin@gmail.com>
2021-11-16 10:16:30 -0600

    Modules Implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { resolve } from 'path';
 import { mongoFactory } from './config/database.config';
 
 import { AdminModule } from './tables/admin/admin.module';
+import { AgencyModule } from './tables/agency/agency.module';
+import { CreditRequestModule } from './tables/creditrequest/creditrequest.module';
 import { GuestModule } from './tables/guest/guest.module';
+import { InspectionAppointmentModule } from './tables/inspectionappointment/inspectionappointment.module';
 import { InsuranceRequestsModule } from './tables/insurancerequests/insurancerequests.module';
+import { NewCarModule } from './tables/newcar/newcar.module';
 import { ReservationsModule } from './tables/reservations/reservations.module';
 import { TestDriveAppointmentsModule } from './tables/testdriveappointments/testdriveappointments.module';
+import { UsedCarModule } from './tables/usedcar/usercar.module';
 import { UserModule } from './tables/user/user.module';
 
 @Module({
@@ -23,10 +28,15 @@ import { UserModule } from './tables/user/user.module';
     }),
 
     AdminModule,
+    AgencyModule,
+    CreditRequestModule,
     GuestModule,
+    InspectionAppointmentModule,
     InsuranceRequestsModule,
+    NewCarModule,
     ReservationsModule,
     TestDriveAppointmentsModule,
+    UsedCarModule,
     UserModule,
   ]
 })
