00d96f512f56d28e3304e23842a7db94fa9849c3
FSolM <fersolmarin@gmail.com>
2021-12-07 18:28:27 -0600

    Global Middleware Implemented

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 // Only for dev
 require('dotenv').config()
 
-const port = process.env.PORT;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const port = process.env.PORT;
+
+  app.useGlobalPipes( new ValidationPipe({ transform: true }));
+
   await app.listen(port || 3000);
 }
 
