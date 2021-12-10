ce251c7c0b0b94ea9428ca426733ff3e57a2ed1a
FSolM <fersolmarin@gmail.com>
2021-12-10 11:32:35 -0600

    Agency Repository Implemented

--- src/entities/agency/agency.module.ts
+++ src/entities/agency/agency.module.ts
@@ -4,11 +4,12 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { Agency } from './model/agency.model';
 import { AgencyController } from './controller/agency.controller';
 import { AgencyService } from './service/agency.service';
+import { AgencyRepository } from './repository/agency.repository';
 
 @Module({
   imports: [TypegooseModule.forFeature([Agency])],
   controllers: [AgencyController],
-  providers: [AgencyService],
+  providers: [AgencyService, AgencyRepository],
 })
 
 export class AgencyModule {}
--- src/entities/agency/service/agency.service.ts
+++ src/entities/agency/service/agency.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from 'nestjs-config';
 
 import { CrudService } from '../../../common/crud/crud.service';
 
 import { Agency } from '../model/agency.model';
+import { AgencyRepository } from '../repository/agency.repository';
 
 @Injectable()
-export class AgencyService extends CrudService<Agency> {}
+export class AgencyService extends CrudService<Agency> {
+  constructor(
+    readonly repository: AgencyRepository,
+    readonly config: ConfigService,
+  ) {
+    super(repository, 'Agency', config);
+  }
+};
