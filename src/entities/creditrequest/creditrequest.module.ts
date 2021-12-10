431dfecb02cda54ec802082c13de094e56bc27a6
FSolM <fersolmarin@gmail.com>
2021-12-10 11:32:48 -0600

    Credit Request Repository Implemented

--- src/entities/creditrequest/creditrequest.module.ts
+++ src/entities/creditrequest/creditrequest.module.ts
@@ -4,11 +4,12 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { CreditRequest } from './model/creditrequest.model';
 import { CreditRequestService } from './service/creditrequest.service';
 import { CreditRequestController } from './controller/creditrequest.controller';
+import { CreditRequestRepository } from './repository/creditrequest.repository';
 
 @Module({
   imports: [TypegooseModule.forFeature([CreditRequest])],
   controllers: [CreditRequestController],
-  providers: [CreditRequestService],
+  providers: [CreditRequestService, CreditRequestRepository],
 })
 
 export class CreditRequestModule {}
--- src/entities/creditrequest/service/creditrequest.service.ts
+++ src/entities/creditrequest/service/creditrequest.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from 'nestjs-config';
 
 import { CrudService } from '../../../common/crud/crud.service';
 
 import { CreditRequest } from '../model/creditrequest.model';
+import { CreditRequestRepository } from '../repository/creditrequest.repository';
 
 @Injectable()
-export class CreditRequestService extends CrudService<CreditRequest> {}
+export class CreditRequestService extends CrudService<CreditRequest> {
+  constructor(
+    readonly repository: CreditRequestRepository,
+    readonly config: ConfigService,
+  ) {
+    super(repository, 'CreditRequest', config);
+  }
+};
