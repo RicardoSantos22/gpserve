6c1743ae11f2e7b39dd638a9476943c700bfd316
Hajnal <farid@kalyptio.com>
2022-01-19 20:25:06 -0600

    entities for car catalogue based on SAD's service response data shape

---
+++ src/entities/newcar/entities/sad-newcar.ts
@@ -0,0 +1,23 @@
+export class SADNewCar {
+    ID: string;
+    agencyID: number;
+    brand: string;
+    model: string;
+    version: string;
+    description: string;
+    technicalFile: string;
+    year: string;
+    color: string;
+    isReserved: string;
+    price: string;
+    fuelType: string;
+    serialNumber: string;
+    chassisType: string;
+    cylinderNumber: string;
+    transmision: string;
+    isAvailable: string;
+    agencyCity: string;
+    demo: string;
+    images: {imageUrl: string}[]
+    specs: {spec: string, descriptionSpec: string}[]
+} 
