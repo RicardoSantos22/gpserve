4ba0280429bee424ae0c36bd00dcbd9f007c585e
Ricardo Santos <ricardo@kalyptio.com>
2022-11-28 16:01:13 -0500

    delete console

--- src/entities/newcar/controller/newcar.controller.ts
+++ src/entities/newcar/controller/newcar.controller.ts
@@ -157,7 +157,6 @@ export class NewCarController {
 
   @Post('setup')
   async getCarCatalogue(@Headers('Authorization') authHeader: string) {
-    console.log(authHeader);
     return this.service.getCarCatalogue(authHeader)
   }
 
