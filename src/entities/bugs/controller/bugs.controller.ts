fd517a0e6ffc3a7e686cc2bb44cc91411a4faf6b
Ricardo Santos <ricardo@kalyptio.com>
2024-06-19 18:17:12 -0500

    update

--- src/entities/bugs/controller/bugs.controller.ts
+++ src/entities/bugs/controller/bugs.controller.ts
@@ -22,22 +22,11 @@ import {
 } from '@nestjs/swagger';
 
 import { BugsService } from '../service/bugs.service';
-import { CreateBugDto } from '../dto/create-bug.dto';
-import { UpdateBugDto } from '../dto/update-bug.dto';
-import { FindAllBugsQuery } from '../dto/findall-query';
+
 
 @Controller('bugs')
 export class BugsController {
   constructor(private readonly bugsService: BugsService) {}
 
-  @Post()
-  create(@Body() createBugDto: CreateBugDto) {
-    return this.bugsService.create(createBugDto);
-  }
-
-  @Get()
-  findAll(@Query() query: FindAllBugsQuery,  @Req() req: Request) {
-    return this.bugsService.findAll(query);
-  }
-
+ 
 }
