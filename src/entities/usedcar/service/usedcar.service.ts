4919da94a2ca5ad7e529cdd237f8eb5fe0268fd3
Ricardo Santos <ricardo@kalyptio.com>
2024-06-28 17:55:48 -0500

    update

--- src/entities/usedcar/service/usedcar.service.ts
+++ src/entities/usedcar/service/usedcar.service.ts
@@ -791,7 +791,7 @@ export class UsedCarService extends CrudService<typeof x> {
 
                             if (BDID !== '') {
 
-                                // await this.repository.update(BDID, usedCar)
+                                await this.repository.update(BDID, usedCar)
                                 updateitem++
                             } else {
                                 usedCarsArray.push(usedCar)
@@ -864,7 +864,7 @@ export class UsedCarService extends CrudService<typeof x> {
                 });
             }
             
-            // const createdCars = await this.repository.createMany(usedCarsArray)
+            const createdCars = await this.repository.createMany(usedCarsArray)
 
             let cars = await this.repository.findAll();
 
